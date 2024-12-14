fb2b11105ae0f2fe2dfe66cb12fba32818061f49
ClydeNoIQ <86055764+ClydeNoIQ@users.noreply.github.com>
2024-12-14 17:46:15 +0800

    Patched OmniSprint

--- src/checks/moving/OmniSprint.php
+++ src/checks/moving/OmniSprint.php
@@ -1,32 +1,5 @@
 <?php
 
-/*
- *
- *  ____           _            __           _____
- * |  _ \    ___  (_)  _ __    / _|  _   _  |_   _|   ___    __ _   _ __ ___
- * | |_) |  / _ \ | | | '_ \  | |_  | | | |   | |    / _ \  / _` | | '_ ` _ \
- * |  _ <  |  __/ | | | | | | |  _| | |_| |   | |   |  __/ | (_| | | | | | | |
- * |_| \_\  \___| |_| |_| |_| |_|    \__, |   |_|    \___|  \__,_| |_| |_| |_|
- *                                   |___/
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zuri attempts to enforce "vanilla Minecraft" mechanics, as well as preventing
- * players from abusing weaknesses in Minecraft or its protocol, making your server
- * more safe. Organized in different sections, various checks are performed to test
- * players doing, covering a wide range including flying and speeding, fighting
- * hacks, fast block breaking and nukers, inventory hacks, chat spam and other types
- * of malicious behaviour.
- *
- * @author ReinfyTeam
- * @link https://github.com/ReinfyTeam/
- *
- *
- */
-
 declare(strict_types=1);
 
 namespace ReinfyTeam\Zuri\checks\moving;
@@ -42,54 +15,83 @@ use ReinfyTeam\Zuri\checks\Check;
 use ReinfyTeam\Zuri\player\PlayerAPI;
 use ReinfyTeam\Zuri\utils\discord\DiscordWebhookException;
 use ReinfyTeam\Zuri\utils\MathUtil;
-use function spl_object_id;
 
 class OmniSprint extends Check {
-	public function getName() : string {
-		return "OmniSprint";
-	}
 
-	public function getSubType() : string {
-		return "A";
-	}
+    private array $playerFlags = [];
 
-	public function maxViolations() : int {
-		return 10;
-	}
+    public function getName(): string {
+        return "OmniSprint";
+    }
 
-	private array $check = [];
+    public function getSubType(): string {
+        return "A";
+    }
 
-	/**
-	 * @throws DiscordWebhookException
-	 */
-	public function check(DataPacket $packet, PlayerAPI $playerAPI) : void {
-		$player = $playerAPI->getPlayer();
-		if ($packet instanceof PlayerAuthInputPacket) {
-			if ($packet->getInputMode() === InputMode::MOUSE_KEYBOARD || $packet->getInputMode() === InputMode::TOUCHSCREEN) { // for windows and mobile, ios only..
-				$left = ($packet->getInputFlags() & (1 << PlayerAuthInputFlags::LEFT)) !== 0;
-				$right = ($packet->getInputFlags() & (1 << PlayerAuthInputFlags::RIGHT)) !== 0;
-				$down = ($packet->getInputFlags() & (1 << PlayerAuthInputFlags::DOWN)) !== 0;
-				if ($down || $right || $left) {
-					if (!$player->isSprinting() && isset($this->check[spl_object_id($playerAPI)])) {
-						$this->failed($playerAPI);
-					}
-					$this->debug($playerAPI, "inputFlag=" . $packet->getInputFlags() . ", inputMode=" . $packet->getInputMode() . ", check=" . isset($this->check[spl_object_id($playerAPI)]));
-				}
-			}
-		}
-	}
+    public function maxViolations(): int {
+        return 10;
+    }
 
-	public function checkEvent(Event $event, PlayerAPI $playerAPI) : void {
-		if ($event instanceof PlayerMoveEvent) {
-			$player = $playerAPI->getPlayer();
-			if (($d = MathUtil::XZDistanceSquared($event->getFrom(), $event->getTo())) > $this->getConstant("max-speed") && !$player->getEffects()->has(VanillaEffects::SPEED())) {
-				$this->check[spl_object_id($playerAPI)] = true; // moving too fast?
-			} else {
-				if (isset($this->check[spl_object_id($playerAPI)])) {
-					unset($this->check[spl_object_id($playerAPI)]);
-				}
-			}
-			$this->debug($playerAPI, "speed=$d, isSprinting=" . $player->isSprinting());
-		}
-	}
+    /**
+     * Checks player input flags for suspicious behavior.
+     */
+    public function check(DataPacket $packet, PlayerAPI $playerAPI): void {
+        if (!$packet instanceof PlayerAuthInputPacket) {
+            return;
+        }
+
+        $inputMode = $packet->getInputMode();
+        if (!in_array($inputMode, [InputMode::MOUSE_KEYBOARD, InputMode::TOUCHSCREEN], true)) {
+            return;
+        }
+
+        $player = $playerAPI->getPlayer();
+        $inputFlags = $packet->getInputFlags();
+
+        $movingLaterally = ($inputFlags & (1 << PlayerAuthInputFlags::LEFT)) !== 0
+            || ($inputFlags & (1 << PlayerAuthInputFlags::RIGHT)) !== 0
+            || ($inputFlags & (1 << PlayerAuthInputFlags::DOWN)) !== 0;
+
+        if ($movingLaterally && !$player->isSprinting()) {
+            $playerId = spl_object_id($playerAPI);
+            if (isset($this->playerFlags[$playerId])) {
+                $this->failed($playerAPI);
+            } else {
+                $this->playerFlags[$playerId] = true;
+            }
+
+            $this->debug($playerAPI, sprintf(
+                "inputFlags=%d, inputMode=%d, checkExists=%s",
+                $inputFlags,
+                $inputMode,
+                isset($this->playerFlags[$playerId]) ? "true" : "false"
+            ));
+        }
+    }
+
+    /**
+     * Checks player movement speed for irregularities.
+     */
+    public function checkEvent(Event $event, PlayerAPI $playerAPI): void {
+        if (!$event instanceof PlayerMoveEvent) {
+            return;
+        }
+
+        $player = $playerAPI->getPlayer();
+        $distanceSquared = MathUtil::XZDistanceSquared($event->getFrom(), $event->getTo());
+        $maxSpeed = $this->getConstant("max-speed");
+
+        $playerId = spl_object_id($playerAPI);
+        if ($distanceSquared > $maxSpeed && !$player->getEffects()->has(VanillaEffects::SPEED())) {
+            $this->playerFlags[$playerId] = true;
+        } else {
+            unset($this->playerFlags[$playerId]);
+        }
+
+        $this->debug($playerAPI, sprintf(
+            "speed=%.2f, isSprinting=%s",
+            $distanceSquared,
+            $player->isSprinting() ? "true" : "false"
+        ));
+    }
 }
